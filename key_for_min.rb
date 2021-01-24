907ccb5053907e71c127ba7d892a871073957cbc
friendly-format-0074 <samuelagasi@gmail.com>
2021-01-24 21:54:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,11 @@ def key_for_min_value(name_hash)
   highest_val= 9999999
 
   name_hash.each do |key, value|
+
     if name_hash[key]< highest_val
       highest_val= name_hash[key]
       highest_key= key
     end
   end
-  puts highest_val
+  puts highest_key
 end
