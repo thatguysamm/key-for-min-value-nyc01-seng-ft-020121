08dcff290a6060bc1efe89819b6e80af2105248a
friendly-format-0074 <samuelagasi@gmail.com>
2021-01-24 21:33:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-key_for_min_value(name_hash)
+  high_val= name_hash[0][0]
+  puts high_val
 
 end
